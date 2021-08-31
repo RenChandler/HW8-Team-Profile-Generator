49d18e0ccdd6ef87920ad7c43cb5917652713bd0
Ren <rnchandramouli42@gmail.com>
2021-08-31 15:49:09 -0600

    Created index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const Manager= require("./lib/Manager")
+const Intern = require("./lib/Intern")
+const Engineer = require("./lib/Engineer")
+
+const inquire = require("inquirer")
+const fs = require("fs")
+const path = require("path")
