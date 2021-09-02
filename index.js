0ba7a429c25a078ac1efc25e9b6a2c391329a96c
Ren <rnchandramouli42@gmail.com>
2021-09-02 00:52:12 -0600

    Finished employee input

--- index.js
+++ index.js
@@ -5,3 +5,161 @@ const Engineer = require("./lib/Engineer")
 const inquire = require("inquirer")
 const fs = require("fs")
 const path = require("path")
+
+const team = [];
+
+const addManager = () => {
+    return inquierer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the manager?', 
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ("What is the name of the manager?");
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is their id?', 
+            validate: nameInput => {
+                if (isNaN(nameInput)) {
+                    console.log ("What is their id?");
+                    return false; 
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is their email?', 
+            validate: email => {
+                valid =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
+                    return true;
+                } else {
+                    console.log ("What is their email?");
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is their office number?', 
+            validate: nameInput => {
+                if (isNaN(nameInput)) {
+                    console.log ("What is their office number?");
+                    return false; 
+                    
+                } 
+                else {
+                    return true;
+                }
+            }
+        },
+    ]).then(managerInput => {
+        const {name,id, email, officeNumber}= managerInput
+        const manager = new Manager(name, id, email, officeNumber)
+
+        team.push(manager)
+        console.log("manager")
+
+    })
+};
+const addEmployee = () => {
+    return inquierer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: 'What is the role of the employee'
+            choices: ['Engineer','Intern']
+        }
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the employee?', 
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ("What is the name of the employee?");
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is their id?', 
+            validate: nameInput => {
+                if (isNaN(nameInput)) {
+                    console.log ("What is their id?");
+                    return false; 
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is their email?', 
+            validate: email => {
+                valid =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
+                    return true;
+                } else {
+                    console.log ("What is their email?");
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is their gibhub username?', 
+            validate: nameInput => {
+                if (isNaN(nameInput)) {
+                    console.log ("What is their username?");
+                    return false; 
+                    
+                } 
+                else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Where did they go to school?', 
+            validate: nameInput => {
+                if (isNaN(nameInput)) {
+                    console.log ("Where did they go to school?");
+                    return false; 
+                    
+                } 
+                else {
+                    return true;
+                }
+            }
+        },
+    ]).then(managerInput => {
+        const {name,id, email, officeNumber}= managerInput
+        const manager = new Manager(name, id, email, officeNumber)
+
+        team.push(manager)
+        console.log("manager")
+
+    })
+};
+
+
